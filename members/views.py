2ac6503a1631cc6820c2e33f34ece2efb78488c0
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-29 21:10:15 +0300

    Initial commit

--- members/views.py
+++ members/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth  import authenticate, login, logout
+from django.contrib.auth  import authenticate, login, logout 
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from django.contrib .auth.forms import UserCreationForm
 from django.contrib import messages
 # Create your views here.
 
@@ -18,3 +20,40 @@ def login_user(request):
             return redirect('login')
     else:
       return render(request, 'authenticate/login.html',{})
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You are Logged Out')
+    return redirect('my_display')
+
+def register_user(request):
+    register(request)
+    messages.success(request, 'You are Logged Out')
+    return redirect('my_display')
+
+# def register_user(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Registration Successful!!!')
+#             return redirect('login')
+#         else:
+#             messages.error(request, 'Registration Failed!!!')
+#             return redirect('register')
+#     else:
+#         form = UserCreationForm()
+#         return render(request, 'authenticate/register.html', {'form': form})
+
+def register_user(request):
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username,password = password)
+            login(request, user)
+            messages.success(request, 'Registration Successful!!!')
+            return redirect('my_index')
+        else:
+            form = UserCreationForm(request.POST)
+    return render(request,'authenticate/register.html',{'form':form})
