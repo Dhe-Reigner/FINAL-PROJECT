424ffb8720ec0f8e7b69e2b948fb27adb53dd552
Dhe-Reigner <martinkagema01@gmail.com>
2024-12-01 16:09:02 +0300

    Initial commit

--- FinalProject/mixins.py
+++ FinalProject/mixins.py
@@ -76,3 +76,68 @@ class AjaxFormMixin(object):
         return response
     
     
+def Directions(*args, **kwargs):
+    '''Handles directions from Google'''
+    lat_a = kwargs.get("lat_a")
+    long_a = kwargs.get("long_a")
+    lat_b = kwargs.get("lat_b")
+    long_b = kwargs.get("long_b")
+    lat_c = kwargs.get("lat_c")
+    long_c = kwargs.get("long_c")
+    lat_d = kwargs.get("lat_d")
+    long_d = kwargs.get("long_d")
+    
+    origin = f"{lat_a},{long_a}"
+    destination = f"{lat_b},{long_b}"
+    waypoints = f"{lat_c},{long_c}|{lat_d},{long_d}"
+    
+    # response = requests.get(url)
+    # url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&waypoints={waypoints}&key={api_key}"
+    # api_key = settings.GOOGLE_MAPS_API_KEY
+
+    result = requests.get(
+        'https://maps.googleapis.com/maps/api/directions/json?',
+        params={
+            'origin': origin,
+            'destination': destination,
+            'waypoints': waypoints,
+            'key': settings.GOOGLE_MAPS_API_KEY
+    })
+    directions = result.json()
+    
+    if directions["status"] == "OK":
+        
+        routes = directions["routes"][0]["legs"]
+        
+        distance = 0
+        duration = 0
+        route_list = []
+        
+        for route in range(len(routes)):
+            
+            distance += int(routes[route]["distance"]["value"])
+            duration += int(routes[route]["duration"]["value"])
+            
+            route_step  ={
+                'origin':routes[route]["start_address"],
+                'destination':routes[route]["end_address"],
+                'distance': routes[route]["distance"]["text"],
+                'duration': routes[route]["duration"]["text"], 
+                
+                'steps':[
+                    [
+                    a["distance"]["text"],
+                    a["duration"]["text"],
+                    a["html_instructions"], 
+                ]
+                for a in routes[route]["steps"]]
+            }
+            
+    return {
+        "origin":origin,
+        "destination":destination,
+        "distance": f"{round(distance/1000, 2)} Km",
+        "duration": format_timespan(duration),
+        "route": route_list,
+        "status": directions["status"]
+    }
--- members/urls.py
+++ members/urls.py
@@ -1,10 +1,23 @@
-from django.urls import path
-from .import views
+# from django.urls import path
+# from .import views
 
+
+# urlpatterns = [
+#    path('login_user',views.login_user,name='login'),
+#    path('logout_user',views.logout_user,name='logout'),
+#    path('register_user',views.register_user,name='register'),
+# ]
+
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-   path('login_user',views.login_user,name='login'),
-   path('logout_user',views.logout_user,name='logout'),
-   path('register_user',views.register_user,name='register'),
+    path('', views.AccountView.as_view(), name='account'),  # default view for users to see their account page
+    path('profile', views.Profile_view, name='profile'),
+    path('sign-up', views.SignUpView.as_view(), name='sign-up'),
+    path('sign-in', views.SignInView.as_view(), name='sign-in'),
+    path('sign-out', views.sign_out, name='sign-out'),
+
+
 ]
- 
