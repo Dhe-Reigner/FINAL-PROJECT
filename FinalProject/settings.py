a2cc25750f0e7b9a9eba692f9530f54af0bb8621
Dhe-Reigner <martinkagema01@gmail.com>
2024-12-05 19:19:36 +0300

    Additional commit

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(q4t6x@%!7n+b7!qhpu$o)gzeeqg$i(l1a%k@^jck7h8t5f)@g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
